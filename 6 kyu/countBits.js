2e930eee5d574f3870e3cdf917461424294bcd43
Shadnev Adam <84129656+ShADAMoV@users.noreply.github.com>
2022-12-20 19:45:57 +0300

    Create countBits.js

---
+++ 6 kyu/countBits.js
@@ -0,0 +1,19 @@
+/*
+Write a function that takes an integer as input, and returns the number of 
+bits that are equal to one in the binary representation of that number. 
+You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+var countBits = function(n) {
+  let arr = n.toString(2);
+  let counter = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '1') {
+      counter++;
+    }
+  }
+  return counter;
+};
